dfc6f17bbf0fae21cd2d656c0ccbf1f9521b30c3
Akanksha2007 <neevsharma381@gmail.com>
2026-02-27 15:47:45 +0530

    UC2: Implement hardcoded palindrome check logic

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,5 +7,16 @@ public class PalindromeCheckerApp {
         System.out.println("Version: " + APP_VERSION);
         System.out.println("Status: System Ready...");
         System.out.println("----------------------------------------");
+        System.out.println("--- Palindrome Checker App v1.1 ---");
+        String original = "madam";
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+        if (original.equals(reversed)) {
+            System.out.println("The word '" + original + "' is a Palindrome.");
+        } else {
+            System.out.println("The word '" + original + "' is NOT a Palindrome.");
+        }
     }
 }
